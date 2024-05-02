0afcda850daa4941fa23e3438080189f58196b22
Hyunseok Byun <gumyoincirno@gmail.com>
2024-05-03 05:39:08 +0900

    Change enviroments

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    images: {
+        domains: ['img2.quasarzone.com'],
+    },
+};
 
 export default nextConfig;
