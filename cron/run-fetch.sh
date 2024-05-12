f19cf20729a2005e48a67be14ba125f716df7f8c
Hyunseok Byun <gumyoincirno@gmail.com>
2024-05-13 00:52:21 +0900

    Fix files related crontab

--- cron/run-fetch.sh
+++ cron/run-fetch.sh
@@ -2,5 +2,5 @@ delay=$((RANDOM % 10 + 1))
 echo "Sleep $delay minutes for refresh data..."
 sleep "${delay}m"
 echo "Start fetch data..."
-node ./fetchData.js
+node /home/BCrawler/cron/fetch-data.js
 echo "Fetch data done."
