f61a67d9161597b3ddf97504e1be93aca8016ee6
Hyunseok Byun <gumyoincirno@gmail.com>
2024-09-14 23:02:45 +0900

    Remove unused files

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button'
+import { Button } from '@shared/ui/button'
 import { ArrowUpLeftFromSquareIcon, BirdIcon } from 'lucide-react'
 import Link from 'next/link'
 
